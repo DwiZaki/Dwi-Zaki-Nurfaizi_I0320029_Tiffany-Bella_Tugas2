2c8c592e9178bb6eda2ddbe86c3dcc3bde36351b
DwiZaki <dwi.zaki.123@gmail.com>
2021-03-11 14:09:37 +0700

    exercise 2.3

---
+++ I0320029_exercise 2.3.py
@@ -0,0 +1,15 @@
+# Menampilkan judul
+print("------Luas dan keliling Lingkaran------")
+
+# input jari-jari
+r = float(input("Masukkan nilai jari-jari: "))
+
+# penghitungan luas
+luas_lingkaran = 3.14 * (r**2)
+
+# penghitungan keliling
+keliling_lingkaran = 2 * 3.14 * r
+
+# Menampilkan
+print("Luas lingkaran = ", luas_lingkaran)
+print("Keliling lingkaran =", keliling_lingkaran)
