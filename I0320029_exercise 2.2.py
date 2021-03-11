6ba00a161d1f4558fc7276b48da3b0e259d68a53
DwiZaki <dwi.zaki.123@gmail.com>
2021-03-11 14:01:48 +0700

    exercise 2.2

---
+++ I0320029_exercise 2.2.py
@@ -0,0 +1,28 @@
+#proses memasukan data ke dalam variabel
+nama  =  "Dwi Zaki Nurfaizi" #proses mencetak variabel print(nama)
+
+#nilai  dan  tipe data dalam variabel	dapat diubah
+umur  =  18	                #nilai  awal
+print(umur)	                #mencetak nilai umur
+print(type(umur))           #mencetak tipe data
+umur = "Delapan Belas"	
+print(umur)                 
+print(type(umur))	        
+
+namaDepan = "Dwi"
+namaBelakang = "Zaki Nurfaizi"
+nama = namaDepan + " " + namaBelakang
+umur = 22
+hobi = "Main basket"
+print("Biodata\n",nama,"\n",umur,"\n",hobi)
+
+#contoh  variabel  lainya 
+inivariabel = "Halo"
+ini_juga_variabel = "Halo juga"
+_inivariabeljuga = "Selamat datang" 
+inivariabel222 = "bye"
+
+p = 15
+l = 12
+luas = p * l
+print(luas)
