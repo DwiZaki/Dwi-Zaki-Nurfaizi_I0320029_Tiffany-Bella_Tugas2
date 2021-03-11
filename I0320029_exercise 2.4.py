c16923a39f545f14736187c20e5105d47b2b1e12
DwiZaki <dwi.zaki.123@gmail.com>
2021-03-11 14:15:17 +0700

    exercise 2.4

---
+++ I0320029_exercise 2.4.py
@@ -0,0 +1,11 @@
+# Menampilkan judul
+print("Konversi suhu (Fahrenheit ke Celcius")
+
+# Memasukkan suhu
+f = float(input("Masukkan suhu Fahrenheit: "))
+
+# Konversi ke celcius
+konversi = 5/9 * (f-32)
+
+# Tampilkan suhu konversi
+print(konversi)
